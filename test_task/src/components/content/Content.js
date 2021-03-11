8fb09ce726e6f859d217ec7f8a33f406193a08dd
zmdog <43880103+zmdog@users.noreply.github.com>
2021-03-11 22:45:56 +0300

    закончил
    вынесу в отдельный блок

--- test_task/src/components/content/Content.js
+++ test_task/src/components/content/Content.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {photo_rendering, movePhotoSelection, zoom_manipulate, $} from "../jq-modules/jq-modules"
+import {photo_rendering, movePhotoSelection, zoom_manipulate, binding_events, change_photo} from "../jq-modules/jq-modules"
 
 
 
@@ -7,56 +7,7 @@ class Content extends React.Component{
 
     componentDidMount(){
         photo_rendering()
-        $(document).ready(function(){
-            let $photo_current = $('.photo-current'),
-                $photo = $photo_current.children('.wrapper-p').children('.photo');
-
-            $photo_current.bind('mousewheel', function(e){
-                let sizeH = $photo.css('height').split('px')[0],
-                    sizeW = $photo.css('width').split('px')[0];
-
-                let width = ($photo.css('width').split('px')[0] / 100) * 2,
-                    height = ($photo.css('height').split('px')[0] / 100) * 2;
-                if(e.originalEvent.wheelDelta /120 > 0) {
-                    $photo.css({height:+sizeH + height + 'px'});
-                    $photo.css({width:+sizeW + width + 'px'})
-                }
-                else{
-                    $photo.css({height:sizeH - height + 'px'});
-                    $photo.css({width:sizeW - width + 'px'})
-                }
-            });
-
-            $photo_current.bind('mouseover', function(e){
-
-                $('body').css({overflow:'hidden'})
-                $(this).css({transition: 'none'})
-            });
-
-            $photo_current.bind('mouseout', function(e){
-                $('body').css({overflow:'overlay'})
-                $(this).css({transition: '1s ease'})
-            });
-
-            $photo_current.bind('mousedown', function(e){
-                let spaceX = e.clientX - $photo.css('left').split('px')[0],
-                    spaceY = e.clientY - $photo.css('top').split('px')[0];
-
-                $photo_current.bind('mousemove', function(e){
-                    let pageX = e.pageX,
-                        pageY = e.pageY,
-                        resultX = pageX - spaceX + 'px',
-                        resultY = pageY - spaceY + 'px';
-                    $photo.css({'left': resultX, 'top':resultY})
-                });
-
-
-            });
-            $photo_current.bind('mouseup', function(e){
-                $('.photo-current').unbind('mousemove');
-            });
-            
-        });
+        binding_events()
     }
     render(){
 
@@ -82,8 +33,8 @@ class Content extends React.Component{
                                     </div>
                                     <div className={"wrapper-dp"}>
                                         <div className={"direction-panel"}>
-                                            <button><i className={"fa fa-angle-left dp"}></i></button>
-                                            <button><i className={"fa fa-angle-right dp"}></i></button>
+                                            <button onClick={(e)=>change_photo('left')}><i className={"fa fa-angle-left dp"}></i></button>
+                                            <button onClick={(e)=>change_photo('right')}><i className={"fa fa-angle-right dp"}></i></button>
                                         </div>
                                     </div>
                                 </div>
--- test_task/src/components/jq-modules/jq-modules.js
+++ test_task/src/components/jq-modules/jq-modules.js
@@ -67,3 +67,65 @@ export let movePhotoSelection = (e)=>{
         if(result >= -20 || result <= -80) return;
         $('.photo-selection').css({left: result + 21 + '%'})
 }
+
+export let binding_events =()=>{
+    $(document).ready(function(){
+        let $photo_current = $('.photo-current'),
+            $photo = $photo_current.children('.wrapper-p').children('.photo');
+
+        $photo_current.bind('mousewheel', function(e){
+            let sizeH = $photo.css('height').split('px')[0],
+                sizeW = $photo.css('width').split('px')[0];
+
+            let width = ($photo.css('width').split('px')[0] / 100) * 2,
+                height = ($photo.css('height').split('px')[0] / 100) * 2;
+            if(e.originalEvent.wheelDelta /120 > 0) {
+                $photo.css({height:+sizeH + height + 'px'});
+                $photo.css({width:+sizeW + width + 'px'})
+            }
+            else{
+                $photo.css({height:sizeH - height + 'px'});
+                $photo.css({width:sizeW - width + 'px'})
+            }
+        });
+
+        $photo_current.bind('mouseover', function(e){
+
+            $('body').css({overflow:'hidden'})
+            $(this).css({transition: 'none'})
+        });
+
+        $photo_current.bind('mouseout', function(e){
+            $('body').css({overflow:'overlay'})
+            $(this).css({transition: '1s ease'})
+        });
+
+        $photo_current.bind('mousedown', function(e){
+            let spaceX = e.clientX - $photo.css('left').split('px')[0],
+                spaceY = e.clientY - $photo.css('top').split('px')[0];
+
+            $photo_current.bind('mousemove', function(e){
+                let pageX = e.pageX,
+                    pageY = e.pageY,
+                    resultX = pageX - spaceX + 'px',
+                    resultY = pageY - spaceY + 'px';
+                $photo.css({'left': resultX, 'top':resultY})
+            });
+
+
+        });
+        $photo_current.bind('mouseup', function(e){
+            $('.photo-current').unbind('mousemove');
+        });
+
+    });
+}
+
+export let change_photo =(direction)=>{
+    let $photo_current = $('.photo-current').children('.wrapper-p').children('.photo'),
+        numb_photo = +$photo_current.attr('src').split('_')[1].split('.')[0];
+
+    if(direction === 'left' && numb_photo - 1 > 0) $photo_current.attr('src', '../public/photos/pic_'+ (numb_photo - 1) +'.jpg');
+
+    if(direction === 'right' && numb_photo + 1 < 12) $photo_current.attr('src', '../public/photos/pic_'+ (numb_photo + 1) +'.jpg')
+}
