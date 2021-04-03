0163b9a9f68361cd85e9daaaae8f79a048d0f240
zmdog <43880103+zmdog@users.noreply.github.com>
2021-04-04 01:01:48 +0300

    Правки в стилях

--- test_task/src/components/footer/Footer.js
+++ test_task/src/components/footer/Footer.js
@@ -1,18 +1,49 @@
 import React from "react";
 
 class Footer extends React.Component {
+
     render() {
+        let text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt' +
+            ' ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris' +
+            ' nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse' +
+            ' cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa' +
+            ' qui officia deserunt mollit anim id est laborum.'
         return (
             <div className={"Footer Wrapper-Footer"}>
                 <div className={"Footer-Background"}>
+                    <div className={"Background"}></div>
                     <div className={"Footer-Info"}>
-                        <div className={"First Footer-Info-First"}>
-                            <div className={"First-AboutCompany"}>О компании</div>
-                            <div className={"First-ForBuyers"}>Для покупателей</div>
-                            <div className={"First-Products"}>Продукты</div>
-                            <div className={"First-CallUs"}>Связаться с нами</div>
+                        <div className={"First Main-Info Footer-Info-First"}>
+                            <div className={"Left First-Right"}>
+                                <div className={"Info Left-AboutCompany"}>
+                                    <div className={"Info-wrapper"}>
+                                        <h>О компании</h>
+                                    </div>
+                                    <p>{text}</p>
+                                </div>
+                                <div className={"Info Left-ForBuyers"}>
+                                    <div className={"Info-wrapper"}>
+                                        <h>Для покупателей</h>
+                                    </div>
+                                    <p>{text}</p>
+                                </div>
+                            </div>
+                            <div className={"Right First-Right"}>
+                                <div className={"Info Right-Products"}>
+                                    <div className={"Info-wrapper"}><
+                                        h>Продукты</h>
+                                    </div>
+                                    <p>{text}</p>
+                                </div>
+                                <div className={"Info Right-CallUs"}>
+                                    <div className={"Info-wrapper"}>
+                                        <h>Связаться с нами</h>
+                                    </div>
+                                    <p>{text}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div className={"Second Footer-Info-Second"}>
+                        <div className={"Second Main-Info Footer-Info-Second"}>
                             <div className={"Second-Wrapper"}>Мы в соцсетях:
                                 <div className={"Socials Second-Socials"}>
                                     <div className={"Social Socials-VK VK"}>
@@ -32,13 +63,14 @@ class Footer extends React.Component {
                             <div className={"Second_Subscribe"}>
                                 <form>
                                     <label>Вы можете подписаться на рассылку</label>
-                                    <input className={"Second-Subscribe-Email"} type={"email"}/>
+                                    <input placeholder={'Abc@gmail.com'} className={"Second-Subscribe-Email"} type={"email"}/>
                                 </form>
                             </div>
                         </div>
+                        <hr/>
+                        <div className={"Thirst Main-Info Footer-Info-Thirst"}>Другая информация</div>
                     </div>
-                    <hr/>
-                    <div>Другая информация</div>
+
                 </div>
             </div>
         )
